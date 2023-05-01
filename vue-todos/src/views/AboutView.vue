37ecf782669071a248df82487e95729d75a32b34
Noam Kritenberg <noamk.hippocampus@gmail.com>
2023-05-01 23:01:18 +0200

    Add About

--- vue-todos/src/views/AboutView.vue
+++ vue-todos/src/views/AboutView.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Todo App using VueJS</h1>
   </div>
+  <p>
+    Hi, my name is Noam Kritenberg and I built this application in order to 
+    learn the basics of VueJS.
+  </p>
+  <p>
+     Special thanks to <a href="https://www.johnkomarnicki.com/"> John
+     Komarnicki </a> for creating lots of VueJS content, I learned a lot from
+     his content.
+  </p> 
 </template>
 
 <style>
@@ -11,5 +20,8 @@
     display: flex;
     align-items: center;
   }
+}
+h1, p {
+    padding: 8px;
 }
 </style>
