90d8914d9404df0a08e04f495d7922cdcbe94f60
Noam Kritenberg <noamk.hippocampus@gmail.com>
2023-05-01 18:12:14 +0200

    Validate no empty todo

--- vue-todos/src/components/TodoCreator.vue
+++ vue-todos/src/components/TodoCreator.vue
@@ -1,14 +1,33 @@
 <template>
-    <div class="input-wrap">
-        <input type="text" v-model="todo"/>
-        <button>Create</button>
+    <div class="input-wrap" :class="{ 'input-err' : todoState.invalid}">
+        <input type="text" v-model="todoState.todo"/>
+        <button @click="createTodo()">Create</button>
     </div>
+    <p v-show="todoState.invalid" class="err-msg">{{ todoState.errorMessage }}</p>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { reactive } from 'vue';
 
-const todo=ref("");
+const emit = defineEmits(["create-todo"]);
+
+const todoState = reactive({
+    todo: "",
+    invalid: false,
+    errorMessage: "",
+});
+
+const createTodo = () => {
+    todoState.invalid = false;
+    if (todoState.todo !== "") {
+        console.log(todoState.todo)
+        emit("create-todo", todoState.todo);
+        todoState.todo = "";
+        return;
+    }
+    todoState.invalid = true;
+    todoState.errorMessage = "Value cannot be empty"
+}
 
 </script>
 
@@ -17,6 +36,9 @@ const todo=ref("");
   display: flex;
   transition: 250ms ease;
   border: 2px solid #41b080;
+  &.input-err {
+    border-color: red;
+  }
   &:focus-within {
     box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
       0 -2px 4px -2px rgb(0 0 0 / 0.1);
@@ -33,5 +55,11 @@ const todo=ref("");
     padding: 8px 16px;
     border: none;
   }
+}
+.err-msg {
+  margin-top: 6px;
+  font-size: 12px;
+  text-align: center;
+  color: red;
 }
 </style>
